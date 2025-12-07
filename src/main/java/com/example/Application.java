d008bb56e50d51f78612c3d97709dd4c74549183
mxvc <410518072@qq.com>
2025-12-07 22:03:54 +0800

    init

--- src/main/java/com/example/Application.java
+++ src/main/java/com/example/Application.java
@@ -1,12 +1,13 @@
 package com.example;
 
+import io.admin.BasePackage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-/**
- * @author 41051
- */
-@SpringBootApplication
+
+@SpringBootApplication(scanBasePackageClasses = {BasePackage.class})
+@EntityScan(basePackageClasses =  {BasePackage.class})
 public class Application {
 
     public static void main(String[] args) {
